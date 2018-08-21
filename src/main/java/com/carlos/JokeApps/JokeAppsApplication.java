8b2efd0b0a87f3f563eac05469f32d52d4604171
19carl <19carl@gmail.com>
2018-08-21 20:59:33 +0530

    JokesApp

---
+++ src/main/java/com/carlos/JokeApps/JokeAppsApplication.java
@@ -0,0 +1,12 @@
+package com.carlos.JokeApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeAppsApplication.class, args);
+	}
+}
